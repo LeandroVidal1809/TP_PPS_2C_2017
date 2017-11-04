bd6257f2d8b237e88cf121d8666d6e5cd5b3eedf
LeandroVidal1809 <leandrovidal92@gmail.com>
2017-11-04 10:16:20 -0300

    Modificaciones en el Home
    Agregamos Botones

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { TomarFotoPage } from '../tomar-foto/tomar-foto';
+import { ExcelPage } from '../excel/excel';
+import { TomarAsistenciaPage } from '../tomar-asistencia/tomar-asistencia';
+import { QRPage } from '../list/list';
+
 import { LoginPage } from '../login/login';
 import { ImportarPage } from '../importar/importar';
 import { MenuController } from 'ionic-angular';
@@ -14,4 +18,24 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
   }
 
+
+  redirect(path:string)
+  {
+    switch (path) {
+        case 'QR':
+        this.navCtrl.push(QRPage);
+        break;
+        case 'Excel':
+        this.navCtrl.push(ExcelPage);
+        break;
+        case 'Camara':
+        this.navCtrl.push(TomarFotoPage);
+        break;
+        case 'Asistencia':
+        this.navCtrl.push(TomarAsistenciaPage);
+        break;  
+    
+
+    }    
+  }
 }
