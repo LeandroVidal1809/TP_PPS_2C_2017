13209b3c29e45976644350d7f4c68bc14a6886b1
Brian Roberts <bri4n95.-@hotmail.com>
2017-11-17 19:44:06 -0300

    push notific
    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,8 @@ import { ListaconsultaPage } from '../pages/listaconsulta/listaconsulta';
 import { AbmAlumnosPage } from '../pages/abm-alumnos/abm-alumnos';
 import { AbmProfyAdminPage } from '../pages/abm-profy-admin/abm-profy-admin';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-// import { EncuestaPage } from '../pages/encuesta/encuesta';
-
+import { EncuestaPage } from '../pages/encuesta/encuesta';
+import { Push } from '@ionic-native/push';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -57,7 +57,7 @@ export const firebaseConfig = {
     AbmAlumnosPage,
     AbmProfyAdminPage,
     ConsultaPage,
-    // EncuestaPage,
+    EncuestaPage,
     ListaconsultaPage
     
   ],
@@ -78,7 +78,7 @@ export const firebaseConfig = {
     QRPage,
     LoginPage,
     GraficosPage,
-    // EncuestaPage,
+     EncuestaPage,
     RegistrarPage,
     TomarAsistenciaPage,
     ListaAsistenciaPage,
@@ -93,7 +93,7 @@ export const firebaseConfig = {
   providers: [
     StatusBar,BarcodeScanner,
     SplashScreen, Camera,
-    Facebook,
+    Facebook,Push,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
