45516cbc90e96a14ad1b6ee3560120dfe4ebdb3f
pabloUTN <pablo.adiliberti@hotmail.com>
2017-11-29 18:06:16 -0300

    info profe
    info profe

---
+++ src/pages/info-profesor/info-profesor.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { InfoProfesorPage } from './info-profesor';
+
+@NgModule({
+  declarations: [
+    InfoProfesorPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(InfoProfesorPage),
+  ],
+})
+export class InfoProfesorPageModule {}
---
+++ src/pages/info-profesor/info-profesor.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angular';
+
+
+
+@IonicPage()
+@Component({
+  selector: 'page-info-profesor',
+  templateUrl: 'info-profesor.html',
+})
+export class InfoProfesorPage {
+
+  emailProf:string;
+  listaProf: any;
+  Email:string;
+  Aula:string;
+  Materia:string;
+  Apellido:string;
+  Nombre:string;
+
+  constructor(public navCtrl: NavController , private view: ViewController,db:AngularFireDatabase, public navParams: NavParams) 
+  {
+
+    this.emailProf = sessionStorage.getItem("EmailProf");
+
+    this.listaProf= db.list('/Profesores/');
+    
+
+          var Observable = this.listaProf.snapshotChanges(['child_added'])
+          .subscribe(actions => {
+            actions.forEach(action => {
+
+            if(action.payload.val()["Email"] == this.emailProf)
+            {  
+           
+                this.Email=action.payload.val()["Email"],
+                this.Aula=action.payload.val()["Aula"],
+                this.Materia=action.payload.val()["Materia"],
+                this.Apellido=action.payload.val()["Apellido"],
+                this.Nombre=action.payload.val()["Nombre"]
+              
+            }
+    
+          });
+      })
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad InfoProfesorPage');
+  }
+  closeModal(){
+    this.view.dismiss();
+      }
+
+}
