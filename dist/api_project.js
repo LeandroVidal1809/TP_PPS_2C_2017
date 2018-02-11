32d08ae96f7c107b7aedd5436916fcb6a410b8ba
LeandroVidal1992 <leandrovidal92@gmail.com>
2018-02-11 11:00:08 -0300

    Add files via upload

---
+++ dist/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "API DOC LEANDRO VIDAL",
+  "version": "1.0.0",
+  "description": "Descripcion de la API para final de PPS. Esta documentación es a modo de ejemplo mostrando al funcionalidad del sistema, no hace referencia directa a una API",
+  "template": {
+    "forceLanguage": "es"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-12-15T19:32:09.939Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
